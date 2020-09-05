40a096ce9c7df0ea9628bf554ec25dd5bcc6d6cb
Artem <killwolftwins@gmail.com>
2020-09-05 12:26:35 +0200

    "prototype" scope annotation added

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,11 +2,13 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.FileOutputStream;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
